85e2c67b84e20d98dbb8d726e38b8890673d5730
David Davies <david.davies@ducttapegames.com>
2020-11-23 00:26:01 +1100

    fixes
    fixed destructible
    fixed shinker button on metroid

--- Assets/Scripts/Submarine/SubDestroyer.cs
+++ Assets/Scripts/Submarine/SubDestroyer.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class SubDestroyer : MonoBehaviour
 {
-    
-    Rigidbody rb;
-    float baseMass;
+    public LayerMask destroy;
+    public Rigidbody rb;
+    public float baseMass;
     // Start is called before the first frame update
     void Awake()
     {
@@ -22,7 +22,8 @@ public class SubDestroyer : MonoBehaviour
 
     void OnCollisionEnter(Collision coll)
     {
-        if(coll.gameObject.layer == LayerMask.NameToLayer("Desctructible") && rb.mass > baseMass)
+            //((layerMask & 1 << layer) != 0)
+        if((destroy & 1 << coll.gameObject.layer) !=0 && rb.mass > baseMass)
         {
             GameObject.Destroy(coll.gameObject);
         }
